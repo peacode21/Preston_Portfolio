f3a2554a1d4b778b16c7942a2c1f2fce751c10cf
Preston <oluwamayowasodipo@gmail.com>
2023-06-30 03:26:47 +0100

    Updated code

---
+++ portfolio/static/js/typed.js
@@ -0,0 +1,3 @@
+var typed = new Typed(".multiple-text",{
+    strings:["Fullstack Developer","Python Developer","Frontend Developer","Backend Developer"],typeSpeed: 100,backSpeed: 100, backDelay: 1000, loop:true
+})
